1ffc1ea3e2327381072f4bfe3f447867c34e7ac7
Ritikesh Machhewar <100903614+ritikesh11052000@users.noreply.github.com>
2025-02-15 08:34:53 +0000

    done

--- Day_2/app_2.py
+++ Day_2/app_2.py
@@ -29,14 +29,26 @@ def download(filepath):
 
 #Visiting /download/images/pic.jpg will return "Downloading file from: images/pic.jpg".
 
-@app.route('/goto/<name>')
-def goto_user(name):
-    return redirect(url_for('user', name=name))
+@app.route('/square/<int:number>')
+def square(number):
+    return f"The square of {number} is {number**2}"
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return "Oops! This page doesn't exist.", 404
+
+#/square/5 → "The square of 5 is 25"
+#/square/abc → 404 error page
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-
+#Summary
+#✅ Dynamic routes allow you to accept variables in URLs.
+#✅ You can specify types like int, float, and path.
+#✅ Multiple parameters can be passed in a single route.
+#✅ Dynamic routes can be used for redirection and file paths.
+#✅ Handling errors properly improves user experience.
 
 
 
